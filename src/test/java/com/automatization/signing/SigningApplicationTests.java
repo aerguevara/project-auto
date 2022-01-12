06bc7c1e4e38e8ff25b8a80c4b8c70ea019afc21
Anyelo Reyes <aerguevara@gmail.com>
2022-01-12 18:21:31 +0100

    update

---
+++ src/test/java/com/automatization/signing/SigningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatization.signing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
